3a80f5ad81bbbc5f830b365f298c5783183e3c77
Sadullah Ecer <sadullahecer@Mac.lan>
2026-01-29 23:19:29 -0800

    Mark notifications seen on bell open

--- src/components/layout/TopBar.jsx
+++ src/components/layout/TopBar.jsx
@@ -74,6 +74,10 @@ export function TopBar({
       profile.setProfileTarget(null);
     } catch (_) {}
 
+    try {
+      touchNotificationsSeen?.();
+    } catch (_) {}
+
     onToggleNotificationsMenu?.();
   };
 
